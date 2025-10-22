08d091b5723a7d6e5e509a2f351926896f434455
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2025-10-22 15:50:32 -0300

    feat: add user infos in user page header

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,12 +1,16 @@
 import AppText from '@/components/AppText';
+import UserData from '@/components/UserData';
+import WorkoutDay from '@/components/WorkoutDay';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { styles } from './styles';
 
 const User = () => {
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <AppText>Olar user!</AppText>
+    <View style={styles.container}>
+      <UserData/>
+      <WorkoutDay/>
     </View>
   );
 };
